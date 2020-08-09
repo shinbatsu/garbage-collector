093b22d39d44e8b7a0186fd382d06700d241244d
shinbatsu <nojic132@gmail.com>
2020-08-09 10:12:33 +0300

    create simple hashfunc test

---
+++ src/test.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "collector.h"
+
+
+#define PRINT_ADDR(var) do { \
+    char *ptr = (char*)&var; \
+    printf("%4s %p   %2x\n", #var, ptr, (*ptr) & 0xff); \
+    ptr++; \
+    for (; ptr<(char*)(&var)+sizeof(var); ptr++) { \
+        printf("     %p   %2x\n", ptr, *ptr & 0xff); \
+    } \
+    printf("-------------------\n"); \
+} while(0)
+
+
+void test() {
+    void *foo = collector_malloc(199);
+}
+
+typedef struct {
+    void *foo;
+    int *bar;
+} HeapStuff;
+
+HeapStuff *newHeapStuff() {
+    HeapStuff *s = collector_malloc(sizeof(HeapStuff));
+    s->foo = collector_malloc(1234);
+    return s;
+}
+
+
+void recursiveAllocationsFunction(int i, HeapStuff *p) {
+    printf(">> recursive allocation function call %d\n", i);
+    p->foo = collector_malloc(sizeof(HeapStuff) + 100);
+    p->bar = collector_malloc(sizeof(int));
+
+    newHeapStuff();
+
+    if (i == 0) return;
+    recursiveAllocationsFunction(i-1, (HeapStuff*)p->foo);
+    *p->bar = 1;
+}
+
+void *bss1 = NULL;
+
+int main(int argc, char **argv) {
+    collector_init();
+    bss1 = collector_malloc(321);
+    static void *bss2;
+    bss2 = collector_malloc(123);
+
+    int *a = (int*)0x0f0f0f0f;
+    void *b = &a;
+
+    for (int i=0; i<100; i++)
+        recursiveAllocationsFunction(10, collector_malloc(sizeof(HeapStuff)));
+
+    char bar = 1;
+    void **p = NULL;
+ 
+    int *foo = (int*)collector_malloc(100*sizeof(int));
+    void *blub = collector_malloc(1000);
+    test();
+
+    HeapStuff *s = newHeapStuff();
+    void *blabla = collector_malloc(23);
+
+    *(int*)bss1 = 1;
+    *(int*)bss2 = 1;
+    collector_exit();
+}
